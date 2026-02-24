e5d4d157c817e6fc4c63e6b3716a7f9e64cd8fa7
kenyoste <113795523+kenyoste@users.noreply.github.com>
2026-02-24 17:29:15 +0300

    Update formatly.js

--- formatly.js
+++ formatly.js
@@ -645,7 +645,16 @@
     advBtn.type = 'button';
     advBtn.className = 'formatly-adv-btn';
     advBtn.title = 'Tam Ekran Düzenle';
-    advBtn.innerHTML = `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="3" width="6" height="2" rx="1" fill="#000000"/><rect x="3" y="3" width="2" height="6" rx="1" fill="#000000"/><rect x="13" y="3" width="6" height="2" rx="1" fill="#000000"/><rect x="17" y="3" width="2" height="6" rx="1" fill="#000000"/><rect x="3" y="17" width="6" height="2" rx="1" fill="#000000"/><rect x="3" y="13" width="2" height="6" rx="1" fill="#000000"/><rect x="13" y="17" width="6" height="2" rx="1" fill="#000000"/><rect x="17" y="13" width="2" height="6" rx="1" fill="#000000"/></svg>`;
+   advBtn.innerHTML = `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="3" y="3" width="6" height="2" rx="1" fill="#2563eb"/>
+      <rect x="3" y="3" width="2" height="6" rx="1" fill="#2563eb"/>
+      <rect x="13" y="3" width="6" height="2" rx="1" fill="#2563eb"/>
+      <rect x="17" y="3" width="2" height="6" rx="1" fill="#2563eb"/>
+      <rect x="3" y="17" width="6" height="2" rx="1" fill="#2563eb"/>
+      <rect x="3" y="13" width="2" height="6" rx="1" fill="#2563eb"/>
+      <rect x="13" y="17" width="6" height="2" rx="1" fill="#2563eb"/>
+      <rect x="17" y="13" width="2" height="6" rx="1" fill="#2563eb"/>
+    </svg>`;
     advBtn.onclick = function(e) {
       e.stopPropagation();
       toolbarDiv.remove();
